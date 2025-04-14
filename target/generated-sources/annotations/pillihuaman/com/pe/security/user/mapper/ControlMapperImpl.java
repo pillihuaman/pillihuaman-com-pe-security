cfe69c32da605ca9a74e5719b8e74c42cd785e4d
zarmirph <zsarmirz2zzarmir@gmail.com>
2025-04-14 07:15:44 -0500

    movil origin

--- target/generated-sources/annotations/pillihuaman/com/pe/security/user/mapper/ControlMapperImpl.java
+++ target/generated-sources/annotations/pillihuaman/com/pe/security/user/mapper/ControlMapperImpl.java
@@ -8,7 +8,7 @@ import pillihuaman.com.pe.security.entity.control.Control;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-06T16:29:03-0500",
+    date = "2025-04-07T22:34:49-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (JetBrains s.r.o.)"
 )
 public class ControlMapperImpl implements ControlMapper {
