35e3cf1812617960de244e2868097d8ca4738d87
zarmirph <zsarmirz2zzarmir@gmail.com>
2025-06-03 23:36:18 -0500

    cambios

---
+++ target/generated-sources/annotations/pillihuaman/com/pe/security/user/mapper/ControlMapperImpl.java
@@ -0,0 +1,49 @@
+package pillihuaman.com.pe.security.user.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import pillihuaman.com.pe.security.dto.RespControl;
+import pillihuaman.com.pe.security.entity.control.Control;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-06-03T11:07:26-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (JetBrains s.r.o.)"
+)
+public class ControlMapperImpl implements ControlMapper {
+
+    @Override
+    public RespControl toRespControl(Control control) {
+        if ( control == null ) {
+            return null;
+        }
+
+        RespControl.RespControlBuilder respControl = RespControl.builder();
+
+        respControl.id( control.getId() );
+        respControl.idCode( control.getIdCode() );
+        respControl.description( control.getDescription() );
+        respControl.icono( control.getIcono() );
+        respControl.iconClass( control.getIconClass() );
+        respControl.status( control.getStatus() );
+        respControl.styleClass( control.getStyleClass() );
+        respControl.text( control.getText() );
+
+        return respControl.build();
+    }
+
+    @Override
+    public List<RespControl> controlsToRespControls(List<Control> controls) {
+        if ( controls == null ) {
+            return null;
+        }
+
+        List<RespControl> list = new ArrayList<RespControl>( controls.size() );
+        for ( Control control : controls ) {
+            list.add( toRespControl( control ) );
+        }
+
+        return list;
+    }
+}
---
+++ target/generated-sources/annotations/pillihuaman/com/pe/security/user/mapper/UserMapperImpl.java
@@ -0,0 +1,46 @@
+package pillihuaman.com.pe.security.user.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import pillihuaman.com.pe.security.dto.RespUser;
+import pillihuaman.com.pe.security.entity.user.User;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-06-03T11:07:26-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (JetBrains s.r.o.)"
+)
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public RespUser toRespUser(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        RespUser.RespUserBuilder respUser = RespUser.builder();
+
+        respUser.id( user.getId() );
+        respUser.alias( user.getAlias() );
+        respUser.mobilPhone( user.getMobilPhone() );
+        respUser.userName( user.getUserName() );
+        respUser.enabled( user.isEnabled() );
+
+        return respUser.build();
+    }
+
+    @Override
+    public List<RespUser> usersToRespUsers(List<User> users) {
+        if ( users == null ) {
+            return null;
+        }
+
+        List<RespUser> list = new ArrayList<RespUser>( users.size() );
+        for ( User user : users ) {
+            list.add( toRespUser( user ) );
+        }
+
+        return list;
+    }
+}
